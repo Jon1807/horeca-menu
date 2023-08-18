016009eb291bdc0a822d3594e74015a54a525be9
IonutNecula <ionutnecula321@gmail.com>
2023-08-18 15:36:54 +0300

    .

--- horeca-menu.php
+++ horeca-menu.php
@@ -41,7 +41,6 @@ function horeca_bio_links_styles() {
     wp_enqueue_style( 'horeca-menu-items', plugins_url( 'assets/css/menu-items.css', __FILE__ ), array(), '1.0.0', 'all' );
     wp_enqueue_style( 'horeca-social-media-links', plugins_url( 'assets/css/social-media-links.css', __FILE__ ), array(), '1.0.0', 'all' );
     wp_enqueue_style( 'bootstrap', plugins_url( 'assets/css/bootstrap.min.css', __FILE__ ), array(), '1.0.0', 'all' );
-    wp_enqueue_script( 'gianniAccordion', plugins_url( 'assets/js/gianniAccordion.min.js', __FILE__), array( 'jquery' ), '1.0', true );
 
 }
 add_action( 'wp_enqueue_scripts', 'horeca_bio_links_styles' );
